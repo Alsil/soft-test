5e67c8f19b2a073a8b900e0249f1f557f88fcc85
Ilya Ivanov <alsirion@gmail.com>
2017-02-28 14:07:22 +0700

    change naming for variables

--- scripts/bar.js
+++ scripts/bar.js
@@ -5,8 +5,8 @@
     var $currentStatusValue = $('.current-status span');
     var $label = $('#label');
     var $labelValue = $('#label span');
-    var currentUSD = 0;
-    var targetUSD = 15;
+    var currentValue = 0;
+    var targetValue = 15;
 
     $.getJSON('http://alex.devel.softservice.org/testapi/', function(data){
         // get initialValue
@@ -14,20 +14,20 @@
         var interval = setInterval(step, 2000);
         function step(){
             // When bar filled
-            if (currentUSD >= initialValue) {
+            if (currentValue >= initialValue) {
                 clearInterval(interval);
                 // If we have initial value more then our target
-                if ( initialValue >= targetUSD ) {
+                if ( initialValue >= targetValue ) {
                     // Change background of target and add text of success
                     $target.addClass('full-bar');
                     $currentStatus.text('We get enough! ($' + initialValue + ')')
                 }
                 // Change status text to how many we still need
                 else {
-                    $currentStatusValue.text('$' + Math.round(targetUSD - currentUSD));
+                    $currentStatusValue.text('$' + Math.round(targetValue - currentValue));
                 }
                 // add label of current initial value under bar
-                if (initialValue < targetUSD) {
+                if (initialValue < targetValue) {
                     $labelValue.text('$' + Math.round(initialValue));
                     $label.addClass('show')
                 }
@@ -36,8 +36,8 @@
             }
             // Bar progression
             else {
-                currentUSD += 0.2;
-                var barWidth = currentUSD * (100/targetUSD);
+                currentValue += 0.2;
+                var barWidth = currentValue * (100/targetValue);
                 if (barWidth >= 100) {
                     barWidth = 100
                 }
